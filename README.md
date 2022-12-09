ab422847377f3108c3d2e75855e3816663c8ee0e
Sozonov Alexey <sozonov.alexey@gmail.com>
2022-12-09 22:19:18 +0300

    Clean up

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
     $ yarn install
     $ yarn start
+
+or 
+
+https://639389362e2d0c1db360896f--delightful-gumption-41e5e1.netlify.app/
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,4 @@
 import { createTheme } from '@mui/material/styles';
-import { LinkProps } from '@mui/material/Link';
 
 const theme = createTheme({
   components: {
